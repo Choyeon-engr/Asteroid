0d4b032eec54f8942b98cae5c0133fb46dfbb3c3
Choyeon <Choyeon.engr@gmail.com>
2020-10-22 19:49:53 +0900

    Create: Actor.cpp

---
+++ Asteroid/Actor/Private/Actor.cpp
@@ -0,0 +1,56 @@
+#include "Actor.hpp"
+#include "Game.hpp"
+#include "Component.hpp"
+
+Actor::Actor(Game* game)
+    : mState(EEnable), mPosition(CML::Vector2D(0.f, 0.f)), mRotation(0.f), mScale(1.f), mGame(game)
+{
+    mGame->AddActor(this);
+}
+
+Actor::~Actor()
+{
+    mGame->RemoveActor(this);
+    
+    while (!mComponents.empty())
+        delete mComponents.back();
+}
+
+void Actor::Update(float deltaTime)
+{
+    if (mState == EEnable)
+    {
+        UpdateComponents(deltaTime);
+        UpdateActor(deltaTime);
+    }
+}
+
+void Actor::UpdateComponents(float deltaTime)
+{
+    for (auto component : mComponents)
+        component->Update(deltaTime);
+}
+
+void Actor::UpdateActor(float deltaTime)
+{
+    
+}
+
+void Actor::AddComponent(Component* component)
+{
+    int updateOrder = component->GetUpdateOrder();
+    auto iter = mComponents.begin();
+    
+    for (; iter != mComponents.end(); ++iter)
+        if (updateOrder < (*iter)->GetUpdateOrder())
+            break;
+    
+    mComponents.insert(iter, component);
+}
+
+void Actor::RemoveComponent(Component* component)
+{
+    auto iter = find(mComponents.begin(), mComponents.end(), component);
+    if (iter != mComponents.end())
+        mComponents.erase(iter);
+}
