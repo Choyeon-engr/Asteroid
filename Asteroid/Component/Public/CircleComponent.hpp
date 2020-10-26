eb4f16fb17ada1967f80d17aa8fa4b12e0853438
Choyeon <Choyeon.engr@gmail.com>
2020-10-26 15:07:00 +0900

    Edit: Move the implementation of Intersect() to cpp file

--- Asteroid/Component/Public/CircleComponent.hpp
+++ Asteroid/Component/Public/CircleComponent.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "Component.hpp"
+#include "Actor.hpp"
 #include "CML.hpp"
 
 class CircleComponent : public Component
 {
 public:
-    CircleComponent(class Actor* owner, int updateOrder) : Component(owner, updateOrder), mRadius(0.f) {}
+    CircleComponent(Actor* owner, int updateOrder) : Component(owner, updateOrder), mRadius(0.f) {}
     
     float GetRadius() const         { return mOwner->GetScale() * mRadius; }
     void SetRadius(float radius)    { mRadius = radius; }
@@ -17,13 +18,4 @@ private:
     float mRadius;
 };
 
-bool Intersect(const CircleComponent& a, const CircleComponent& b)
-{
-    CML::Vector2D d = a.GetCenter() - b.GetCenter();
-    float dSquared = d.LengthSquared();
-    
-    float rSquared = a.GetRadius() + a.GetRadius();
-    rSquared *= rSquared;
-    
-    return dSquared <= rSquared;
-}
+bool Intersect(const CircleComponent& a, const CircleComponent& b);
