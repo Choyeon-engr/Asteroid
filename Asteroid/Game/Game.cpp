a8bfabff59e5d7429b5e0fc6eb7018d08ee5827f
Choyeon <Choyeon.engr@gmail.com>
2020-10-26 15:04:52 +0900

    Feat: Respawn a jet, Spawn asteroids, etc...

--- Asteroid/Game/Game.cpp
+++ Asteroid/Game/Game.cpp
@@ -1,11 +1,18 @@
 #include <SDL2_image/SDL_image.h>
+
 #include "Game.hpp"
 #include "Actor.hpp"
 #include "SpriteComponent.hpp"
+#include "Jet.hpp"
+#include "Asteroid.hpp"
+#include "CML.hpp"
 
 bool Game::Init()
 {
     if (SDL_Init(SDL_INIT_VIDEO))
+        return false;
+    
+    if (!IMG_Init(IMG_INIT_PNG))
         return false;
     
     mWindow = SDL_CreateWindow("Asteroid", 0, 0, 512, 512, 0);
@@ -15,6 +22,12 @@ bool Game::Init()
     mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!mRenderer)
         return false;
+    
+    CML::Random::Init();
+    
+    LoadData();
+    
+    mTicksCount = SDL_GetTicks();
     
     return true;
 }
@@ -31,6 +44,8 @@ void Game::Loop()
 
 void Game::Quit()
 {
+    UnloadData();
+    IMG_Quit();
     SDL_DestroyRenderer(mRenderer);
     SDL_DestroyWindow(mWindow);
     SDL_Quit();
@@ -68,8 +83,8 @@ void Game::AddSprite(SpriteComponent* sprite)
     auto iter = mSprites.begin();
     
     for (; iter != mSprites.end(); ++iter)
-        if (drawOrder < (*iter)->GetDrawOrder())
-            break;
+    if (drawOrder < (*iter)->GetDrawOrder())
+        break;
     
     mSprites.insert(iter, sprite);
 }
@@ -80,16 +95,17 @@ void Game::RemoveSprite(SpriteComponent* sprite)
     mSprites.erase(iter);
 }
 
-SDL_Texture* Game::GetTexture(string& fileName)
+SDL_Texture* Game::GetTexture(const string& fileName)
 {
     SDL_Texture* texture = nullptr;
-    auto iter = mTextures.find(fileName);
+    const string AbsolutePath = string("/Users/choyeon/Game/Asteroid/Asteroid") + fileName;
+    auto iter = mTextures.find(AbsolutePath.c_str());
     
     if (iter != mTextures.end())
         texture = iter->second;
     else
     {
-        SDL_Surface* surface = IMG_Load(fileName.c_str());
+        SDL_Surface* surface = IMG_Load(AbsolutePath.c_str());
         if (!surface)
             return nullptr;
         
@@ -98,10 +114,22 @@ SDL_Texture* Game::GetTexture(string& fileName)
         if (!texture)
             return nullptr;
         
-        mTextures.emplace(fileName.c_str(), texture);
+        mTextures.emplace(AbsolutePath, texture);
     }
     
     return texture;
+}
+
+void Game::AddAsteroid(Asteroid* asteroid)
+{
+    mAsteroids.emplace_back(asteroid);
+}
+
+void Game::RemoveAsteroid(Asteroid* asteroid)
+{
+    auto iter = find(mAsteroids.begin(), mAsteroids.end(), asteroid);
+    if (iter != mAsteroids.end())
+        mAsteroids.erase(iter);
 }
 
 void Game::Input()
@@ -117,6 +145,12 @@ void Game::Input()
                 break;
         }
     }
+    
+    const Uint8* keyState = SDL_GetKeyboardState(nullptr);
+    mIsUpdatingActors = true;
+    for (auto actor : mActors)
+        actor->Input(keyState);
+    mIsUpdatingActors = false;
 }
 
 void Game::Update()
@@ -128,6 +162,35 @@ void Game::Update()
         deltaTime = 0.05f;
     
     mTicksCount = SDL_GetTicks();
+    
+    mIsUpdatingActors = true;
+    for (auto actor : mActors)
+        actor->Update(deltaTime);
+    mIsUpdatingActors = false;
+    
+    for (auto pendingActor : mPendingActors)
+        mActors.emplace_back(pendingActor);
+    mPendingActors.clear();
+    
+    vector<Actor*> disabledActors;
+    for (auto actor : mActors)
+        if (actor->GetState() == Actor::EDisable)
+            disabledActors.emplace_back(actor);
+    
+    for (auto disabledActor : disabledActors)
+        delete disabledActor;
+    
+    if (mIsDead)
+    {
+        if (mRespawnTime <= 0.f)
+        {
+            mJet = new Jet(this);
+            mRespawnTime = 3.f;
+            mIsDead = false;
+        }
+        else
+            mRespawnTime -= deltaTime;
+    }
 }
 
 void Game::Output()
@@ -135,5 +198,28 @@ void Game::Output()
     SDL_SetRenderDrawColor(mRenderer, 0, 50, 100, 255);
     SDL_RenderClear(mRenderer);
     
+    for (auto sprite : mSprites)
+        sprite->Draw(mRenderer);
+    
     SDL_RenderPresent(mRenderer);
+}
+
+void Game::LoadData()
+{
+    mJet = new Jet(this);
+    
+    const int AsteroidsCnt = 5;
+    for (int i = 0; i < AsteroidsCnt; ++i)
+    new Asteroid(this);
+}
+
+void Game::UnloadData()
+{
+    while (!mActors.empty())
+        delete mActors.back();
+    
+    for (auto texture : mTextures)
+        SDL_DestroyTexture(texture.second);
+    
+    mTextures.clear();
 }
--- Asteroid/Game/Game.hpp
+++ Asteroid/Game/Game.hpp
@@ -10,7 +10,7 @@ using namespace std;
 class Game
 {
 public:
-    Game() : mIsRunning(true), mIsUpdatingActors(false), mTicksCount(0), mWindow(nullptr), mRenderer(nullptr) {}
+    Game() : mIsRunning(true), mIsUpdatingActors(false), mTicksCount(0), mWindow(nullptr), mRenderer(nullptr), mIsDead(false), mRespawnTime(3.f) {}
     
     bool Init();
     void Loop();
@@ -22,12 +22,22 @@ public:
     void AddSprite(class SpriteComponent* sprite);
     void RemoveSprite(class SpriteComponent* sprite);
     
-    SDL_Texture* GetTexture(string& fileName);
+    SDL_Texture* GetTexture(const string& fileName);
+    
+    /* Specialization for Asteroid */
+    void AddAsteroid(class Asteroid* asteroid);
+    void RemoveAsteroid(class Asteroid* asteroid);
+    const vector<class Asteroid*>& GetAsteroids() const noexcept { return mAsteroids; }
+    
+    void SetIsDead(bool isDead) { mIsDead = isDead; }
     
 private:
     void Input();
     void Update();
     void Output();
+    
+    void LoadData();
+    void UnloadData();
     
     bool mIsRunning;
     bool mIsUpdatingActors;
@@ -42,4 +52,11 @@ private:
     
     vector<class SpriteComponent*> mSprites;
     unordered_map<string, SDL_Texture*> mTextures;
+    
+    /* Specialization for Asteroid */
+    class Jet* mJet;
+    vector<class Asteroid*> mAsteroids;
+    
+    bool mIsDead;
+    float mRespawnTime;
 };
