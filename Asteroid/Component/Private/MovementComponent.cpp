afaa89686ae5e92c6a9874783df7e9c0bc90b9b2
Choyeon <Choyeon.engr@gmail.com>
2020-10-27 14:04:15 +0900

    Feat: Apply Newtonian physics

--- Asteroid/Component/Private/MovementComponent.cpp
+++ Asteroid/Component/Private/MovementComponent.cpp
@@ -1,27 +1,36 @@
+#include <cmath>
+
 #include "MovementComponent.hpp"
 #include "Actor.hpp"
-#include "CML.hpp"
 
 void MovementComponent::Update(float deltaTime)
 {
-    if (!CML::CloseToZero(mForwardSpeed))
+    mAcceleration = mForce / mMass;
+    mForce = CML::Vector2D(0.f, 0.f);
+    mVelocity += mAcceleration * deltaTime;
+    
+    if (!CML::CloseToZero(sqrt(mVelocity.LengthSquared())))
     {
         CML::Vector2D position = mOwner->GetPosition();
-        position += mOwner->GetForwardVector() * mForwardSpeed * deltaTime;
+        position += mVelocity * deltaTime;
         
-        /* Specialization for Asteroid */
-        if (position.X < 0.f)
-            position.X = 512.f;
-        else if (position.X > 512.f)
-            position.X = 0.f;
-        
-        if (position.Y < 0.f)
-            position.Y = 512.f;
-        else if (position.Y > 512.f)
-            position.Y = 0.f;
+        if (mOwner->GetKinds() == Actor::EAsteroid)
+        {
+            if (position.X < 0.f)
+                position.X = 512.f;
+            else if (position.X > 512.f)
+                position.X = 0.f;
+            
+            if (position.Y < 0.f)
+                position.Y = 512.f;
+            else if (position.Y > 512.f)
+                position.Y = 0.f;
+        }
         
         mOwner->SetPosition(position);
     }
+    
+    mVelocity = CML::Vector2D(0.f, 0.f);
     
     if (!CML::CloseToZero(mAngularSpeed))
     {
