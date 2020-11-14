2b87c5c8cb8e89dbe2f2f5427162d177974fd59a
Choyeon <Choyeon.engr@gmail.com>
2020-11-14 12:36:21 +0900

    Create: VertexArray.hpp

---
+++ Asteroid/VertexArray/VertexArray.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+class VertexArray
+{
+public:
+    VertexArray(const float* vertices, unsigned int numVertices, const unsigned int* indices, unsigned int numIndices);
+    ~VertexArray();
+    
+    void SetActive();
+    
+    unsigned int GetNumVertices() const { return mNumVertices; }
+    unsigned int GetNumIndices() const { return mNumIndices; }
+    
+private:
+    unsigned int mNumVertices;
+    unsigned int mNumIndices;
+    
+    unsigned int mVertexBufferID;
+    unsigned int mIndexBufferID;
+    unsigned int mVertexArrayID;
+};
